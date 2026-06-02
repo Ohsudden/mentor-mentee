f0328c10e872f437f602d7db12395f9f31511f13
Overstar <mrganja122005@gmail.com>
2026-06-02 13:46:15 -0400

    Questionnaire+settings update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
 from starlette.middleware.sessions import SessionMiddleware
+from pydantic import BaseModel
 from pwdlib import PasswordHash
 import os, time, secrets, ast, json, re
 from typing import List, Optional
@@ -14,12 +15,21 @@ app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key='dev_secret')
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+
+templates = Jinja2Templates(directory="static")
 
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request, "index.html")
 
-@app.get("/register", response_class=HTMLResponse)
+@app.get("/registration", response_class=HTMLResponse)
 def read_register(request: Request):
-    return templates.TemplateResponse("register.html", {"request": request})
+    return templates.TemplateResponse(request, "registration.html")
+
+@app.get("/settings", response_class=HTMLResponse)
+def read_settings(request: Request):
+    return templates.TemplateResponse(request, "settings.html")
+
+@app.get("/questionnaire", response_class=HTMLResponse)
+def read_questionnaire(request: Request):
+    return templates.TemplateResponse(request, "questionnaire.html")
